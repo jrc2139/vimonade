7c570bf3b41d02da6f0d293373620543f7fc3381
John Castronuovo <johncas@kth.se>
2020-06-22 16:42:31 +0200

    feat(ip range check): function ip range interceptor

--- pkg/protocol/grpc/server.go
+++ pkg/protocol/grpc/server.go
@@ -2,30 +2,18 @@ package grpc
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/peer"
-
-	"github.com/pocke/go-iprange"
 
 	v1 "github.com/jrc2139/vimonade/pkg/api/v1"
 )
 
-var ra *iprange.Range
-
 // RunServer registers gRPC service and run server.
 func RunServer(ctx context.Context, srv v1.MessageServiceServer, creds credentials.TransportCredentials, allowRange, serverAddr string) error {
 	listen, err := net.Listen("tcp", serverAddr)
-	if err != nil {
-		return err
-	}
-
-	ra, err = iprange.New(allowRange)
 	if err != nil {
 		return err
 	}
@@ -35,7 +23,7 @@ func RunServer(ctx context.Context, srv v1.MessageServiceServer, creds credentia
 
 	if creds == nil {
 		// insecure
-		server = grpc.NewServer(grpc.UnaryInterceptor(IpInterceptor))
+		server = grpc.NewServer()
 	} else {
 		// secure
 		server = grpc.NewServer(grpc.Creds(creds))
@@ -47,19 +35,4 @@ func RunServer(ctx context.Context, srv v1.MessageServiceServer, creds credentia
 	log.Println("starting gRPC server...")
 
 	return server.Serve(listen)
-}
-
-func IpInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-	p, _ := peer.FromContext(ctx)
-	if !ra.IncludeStr(p.Addr.String()) {
-		return nil, errors.New(fmt.Sprintf("not in allow ip range: %s", p.Addr.String()))
-		// http.Error(w, "Not allow ip.", 503)
-		// logger.Info("not in allow ip. from: ", p.Addr.String())
-		// return
-	}
-	// Calls the handler
-	return handler(ctx, req)
-
-	// Invoke 'handler' to use your gRPC server implementation and get
-	// the response.
 }
--- server/server.go
+++ server/server.go
@@ -3,20 +3,67 @@ package server
 import (
 	"context"
 	"fmt"
+	"net"
+	"strings"
 
 	log "github.com/inconshreveable/log15"
+	"github.com/pocke/go-iprange"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/peer"
 
 	"github.com/jrc2139/vimonade/lemon"
-	"github.com/jrc2139/vimonade/pkg/protocol/grpc"
-	v1 "github.com/jrc2139/vimonade/pkg/service/v1"
+	v1 "github.com/jrc2139/vimonade/pkg/api/v1"
+	service "github.com/jrc2139/vimonade/pkg/service/v1"
 )
 
 func Serve(c *lemon.CLI, creds credentials.TransportCredentials, logger log.Logger) error {
-	// Server
-	if err := grpc.RunServer(context.Background(), v1.NewMessageServerService(c.LineEnding, logger), creds, c.Allow, fmt.Sprintf("%s:%d", c.Host, c.Port)); err != nil {
+	listen, err := net.Listen("tcp", fmt.Sprintf("%s:%d", c.Host, c.Port))
+	if err != nil {
 		return err
 	}
 
-	return nil
+	ra, err := iprange.New(c.Allow)
+	if err != nil {
+		return err
+	}
+
+	// register service
+	var server *grpc.Server
+
+	ipInterceptor := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		p, ok := peer.FromContext(ctx)
+		if !ok {
+			logger.Crit("error fetching ip addr from request", nil)
+			return nil, fmt.Errorf("error fetching ip addr from request")
+		}
+
+		ipAndPort := p.Addr.String()
+		ip := strings.Split(ipAndPort, ":")
+
+		if !ra.IncludeStr(ip[0]) {
+			logger.Crit("not in allow ip range", ip[0], ra)
+			return nil, fmt.Errorf("not in allow ip range", ip[0])
+		}
+
+		// Calls the handler
+		return handler(ctx, req)
+	}
+
+	if creds == nil {
+		// insecure
+		server = grpc.NewServer(grpc.UnaryInterceptor(ipInterceptor))
+	} else {
+		// secure
+		server = grpc.NewServer(grpc.Creds(creds), grpc.UnaryInterceptor(ipInterceptor))
+	}
+
+	srv := service.NewMessageServerService(c.LineEnding, logger)
+
+	v1.RegisterMessageServiceServer(server, srv)
+
+	// start gRPC server
+	logger.Debug("starting gRPC server...")
+
+	return server.Serve(listen)
 }
