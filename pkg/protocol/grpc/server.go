d0c22d17e04bf74668ca6de0849aeff6dea2329c
John Castronuovo <johncas@kth.se>
2020-06-22 14:55:20 +0200

    feat(interceptor): ip range allow grpc unary interceptor

--- pkg/protocol/grpc/server.go
+++ pkg/protocol/grpc/server.go
@@ -2,18 +2,30 @@ package grpc
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/peer"
+
+	"github.com/pocke/go-iprange"
 
 	v1 "github.com/jrc2139/vimonade/pkg/api/v1"
 )
 
+var ra *iprange.Range
+
 // RunServer registers gRPC service and run server.
-func RunServer(ctx context.Context, srv v1.MessageServiceServer, creds credentials.TransportCredentials, serverAddr string) error {
+func RunServer(ctx context.Context, srv v1.MessageServiceServer, creds credentials.TransportCredentials, allowRange, serverAddr string) error {
 	listen, err := net.Listen("tcp", serverAddr)
+	if err != nil {
+		return err
+	}
+
+	ra, err = iprange.New(allowRange)
 	if err != nil {
 		return err
 	}
@@ -23,7 +35,7 @@ func RunServer(ctx context.Context, srv v1.MessageServiceServer, creds credentia
 
 	if creds == nil {
 		// insecure
-		server = grpc.NewServer()
+		server = grpc.NewServer(grpc.UnaryInterceptor(IpInterceptor))
 	} else {
 		// secure
 		server = grpc.NewServer(grpc.Creds(creds))
@@ -35,4 +47,19 @@ func RunServer(ctx context.Context, srv v1.MessageServiceServer, creds credentia
 	log.Println("starting gRPC server...")
 
 	return server.Serve(listen)
+}
+
+func IpInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	p, _ := peer.FromContext(ctx)
+	if !ra.IncludeStr(p.Addr.String()) {
+		return nil, errors.New(fmt.Sprintf("not in allow ip range: %s", p.Addr.String()))
+		// http.Error(w, "Not allow ip.", 503)
+		// logger.Info("not in allow ip. from: ", p.Addr.String())
+		// return
+	}
+	// Calls the handler
+	return handler(ctx, req)
+
+	// Invoke 'handler' to use your gRPC server implementation and get
+	// the response.
 }
--- server/server.go
+++ server/server.go
@@ -3,12 +3,8 @@ package server
 import (
 	"context"
 	"fmt"
-	"net"
-	"net/http"
-	"net/url"
 
 	log "github.com/inconshreveable/log15"
-	"github.com/pocke/go-iprange"
 	"google.golang.org/grpc/credentials"
 
 	"github.com/jrc2139/vimonade/lemon"
@@ -16,72 +12,9 @@ import (
 	v1 "github.com/jrc2139/vimonade/pkg/service/v1"
 )
 
-var logger log.Logger
-var lineEnding string
-var ra *iprange.Range
-var port int
-var path = "./files"
-
-func translateLoopbackIP(uri string, remoteIP string) string {
-	parsed, err := url.Parse(uri)
-	if err != nil {
-		return uri
-	}
-
-	host, port, err := net.SplitHostPort(parsed.Host)
-	if err != nil {
-		return err.Error()
-	}
-
-	ip := net.ParseIP(host)
-	if ip == nil || !ip.IsLoopback() {
-		return uri
-	}
-
-	if len(port) == 0 {
-		parsed.Host = remoteIP
-	} else {
-		parsed.Host = fmt.Sprintf("%s:%s", remoteIP, port)
-	}
-
-	return parsed.String()
-}
-
-func middleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet && r.Method != http.MethodPost {
-			http.Error(w, "Not support method.", 404)
-			return
-		}
-
-		remoteIP, _, err := net.SplitHostPort(r.RemoteAddr)
-		if err != nil {
-			http.Error(w, "RemoteAddr error.", 500)
-			return
-		}
-		if !ra.IncludeStr(remoteIP) {
-			http.Error(w, "Not allow ip.", 503)
-			logger.Info("not in allow ip. from: ", remoteIP)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-func Serve(c *lemon.CLI, creds credentials.TransportCredentials, _logger log.Logger) error {
-	logger = _logger
-	lineEnding = c.LineEnding
-	port = c.Port
-
-	var err error
-	ra, err = iprange.New(c.Allow)
-	if err != nil {
-		logger.Error("allowIp error")
-		return err
-	}
-
+func Serve(c *lemon.CLI, creds credentials.TransportCredentials, logger log.Logger) error {
 	// Server
-	if err := grpc.RunServer(context.Background(), v1.NewMessageServerService(c.LineEnding, logger), creds, fmt.Sprintf("%s:%d", c.Host, c.Port)); err != nil {
+	if err := grpc.RunServer(context.Background(), v1.NewMessageServerService(c.LineEnding, logger), creds, c.Allow, fmt.Sprintf("%s:%d", c.Host, c.Port)); err != nil {
 		return err
 	}
 
